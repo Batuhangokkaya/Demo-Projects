3125d7ce54da8a2bb8faa9527316d7c56df7e7ed
Batuhangokkaya <batuhangkk34@gmail.com>
2025-01-17 05:13:44 +0300

    Add project files.

---
+++ ValueAndReferenceType/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ValueAndReferenceType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Değer Tipler    : Integer, Boolean, Inum, Double ve Decimal gibilerdir.
+            Referans Tipler : Classlar, Interfacesler, Abstract sınıflar, String ve Arrayler'dir.
+            Sonradan veri değişmeyecek ise referans almaya gerek yoktur.
+            Referans alındığında Sonradan Veri değişir İse sonuç Değişir.
+            */
+            // Value Type
+            Console.WriteLine("--Value Type--");
+            int number1 = 10;
+            int number2 = 20;
+
+            number2 = number1;
+            number1 = 30;
+
+            Console.WriteLine(number2);
+
+            // Reference Type
+            Console.WriteLine("--Reference Type--");
+            string[] cities1 = new string[] { "Ankara", "İstanbul", "Samsun" };
+            string[] cities2 = new string[] { "Bursa", "Kocaeli", "Balıkesir" };
+
+            cities2 = cities1;
+            cities1[0] = "İzmir";
+            foreach (var city in cities2)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
